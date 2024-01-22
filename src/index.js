82a8fe6292ae91bfabf93bd7286a11d5730fddff
Лилия Новикова <kplily85@gmail.com>
2024-01-22 13:36:28 +0000

    Added full data info for the page

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 const API_KEY = "bcffaa2e3145f0t13bb8e32f07536eoe";
 const BASE_URL = "https://api.shecodes.io/weather";
 
-async function showWeather(query) {
+const setWeather = (weatherData) => {
+  let cityEl = document.querySelector(".current-city");
+  let humidityEl = document.querySelector("#current-humidity");
+  let descriptionEl = document.querySelector("#current-description");
+  let windEl = document.querySelector("#current-wind");
+  let iconEl = document.createElement("img");
+  let iconContainerEl = document.querySelector("#icon");
+  let temperatureEl = document.querySelector(".current-temperature-value");
+
+  console.log(weatherData);
+  cityEl.innerHTML = weatherData.city;
+  descriptionEl.innerHTML = weatherData.condition.description;
+  humidityEl.innerHTML = weatherData.temperature.humidity;
+  windEl.innerHTML = Math.round(weatherData.wind.speed);
+  iconEl.src = weatherData.condition.icon_url;
+  iconEl.alt = weatherData.condition.icon;
+  iconContainerEl.innerHTML = "";
+  iconContainerEl.appendChild(iconEl);
+  temperatureEl.innerHTML = Math.round(weatherData.temperature.current);
+};
+
+const showWeather = async (query = "Paris") => {
   let url = `${BASE_URL}/v1/current?query=${query}&key=${API_KEY}`;
   try {
     const weather = await fetch(url).then((res) => res.json());
 
     if (weather) {
-      let cityEl = document.querySelector(".current-city");
-      let temperatureEl = document.querySelector(".current-temperature-value");
-
-      cityEl.innerHTML = weather.city;
-      temperatureEl.innerHTML = Math.round(weather.temperature.current);
+      setWeather(weather);
     }
   } catch {
-    throw new Error("Faild fetch");
+    throw new Error("Failed fetch");
   }
-}
+};
 
-function search(event) {
+const search = (event) => {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let inputValue = searchInputElement.value;
@@ -26,9 +43,9 @@ function search(event) {
   if (!!inputValue) {
     showWeather(inputValue);
   }
-}
+};
 
-function formatDate(date) {
+const formatDate = (date) => {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let day = date.getDay();
@@ -53,7 +70,7 @@ function formatDate(date) {
 
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
-}
+};
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
